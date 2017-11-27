ef4cfd5045295127c40d63303d20c7ad358dea58
Vovanjsthis <jsthis@ukr.net>
2017-11-27 17:10:28 +0200

    grey line, no button

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ renderData()
 	function renderData(){
 		for(let i=0; i<words.word.length; i++){
 			let idb = "but_" + i;
-			let row = `<td class="td">${words.word[i]}<button id="${idb}" type="button" class="btn btn-default glyphicon glyphicon-volume-up btn-success"></button></td><td class="td">${words.translate[i]}</td><td class="td">${words.komment[i]}</td>`;
+			let td1 = "td_" + i;			
+			let row = `<td id="${td1}" class="td">${words.word[i]}<button id="${idb}" type="button" class="btn btn-default glyphicon glyphicon-volume-up btn-success"></button></td><td class="td">${words.translate[i]}</td><td class="td">${words.komment[i]}</td>`;
 			let tr = document.createElement('tr');
 			let tbody = document.getElementById("tbody");
 tbody.insertBefore(tr, tbody.firstChild);
 //			tbody.appendChild(tr);
 			tr.innerHTML = row;
-			let but = document.getElementById(idb);
-			//console.log(but);		
+			let but = document.getElementById(idb);			
+			let tdw = document.getElementById(td1);			
+			if(tdw.firstChild == but){
+				but.style.visibility = "hidden";
+				but.style.height = "5px";
+				tr.style.backgroundColor = "grey"
+				console.log(tr)
+			};
 			but.addEventListener("click", Say)
 		};
 	};
